576d76ab7d99e06a61d85d27efc761ff09d87ca7
Victor Bonanho - Desenvolvedor - SoftVision <victor.timotti@softvision.com.br>
2022-06-21 23:04:21 -0300

    add search bar(need to fix it)

--- App.js
+++ App.js
@@ -1,91 +1,115 @@
-import React, {useRef, useMemo, useState, useEffect} from 'react';
-import { StyleSheet, Text, View, SafeAreaView, LogBox } from 'react-native';
-import ListItem from './components/ListItem';
+import React, { useRef, useMemo, useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
+import ListItem from "./components/ListItem";
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
-} from '@gorhom/bottom-sheet';
-import { SAMPLE_DATA } from './assets/data/sampleData';
-import { ScrollView, FlatList, GestureHandlerRootView, gestureHandlerRootHOC  } from 'react-native-gesture-handler';
-import Chart from './components/Chart';
-import { getMarketData } from './services/cryptoService';
-import { color } from 'react-native-reanimated';
-
-LogBox.ignoreLogs(["EventEmitter.removeListener"]);
+} from "@gorhom/bottom-sheet";
+import { SAMPLE_DATA } from "./assets/data/sampleData";
+import {
+  FlatList,
+  GestureHandlerRootView,
+  gestureHandlerRootHOC,
+} from "react-native-gesture-handler";
+import Chart from "./components/Chart";
+import { getMarketData } from "./services/cryptoService";
 
 const ListHeader = () => (
   <>
     <View style={styles.titleWrapper}>
       <Text style={styles.largeTitle}>Markets</Text>
     </View>
-    <View style={styles.divider}/>
+    <View style={styles.divider} />
   </>
-)
+);
 
 export default function App() {
   const [data, setData] = useState([]);
   const [selectedCoidData, setSelectedCoinData] = useState(null);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     const fetchMarketData = async () => {
       const marketData = await getMarketData();
       setData(marketData);
-    }
-
+    };
     fetchMarketData();
-  }, [])
+  }, []);
+
+  //Change text logic (problem => not get the data back)
+  useEffect(() => {
+    if (searchText === "") {
+      setData(data);
+    } else {
+      setData(
+        data.filter(
+          (item) =>
+            item.id.toLowerCase().indexOf(searchText.toLocaleLowerCase()) > -1
+        )
+      );
+    }
+  }, [searchText]);
 
   // ref
   const bottomSheetModalRef = useRef(null);
 
   // variables
-  const snapPoints = useMemo(() => ['50%'], []);
+  const snapPoints = useMemo(() => ["50%"], []);
 
   const openModal = (item) => {
     setSelectedCoinData(item);
     bottomSheetModalRef.current.present();
-  }
+  };
 
   return (
-    <GestureHandlerRootView style={{ flex: 1, }}>
-    <BottomSheetModalProvider>
-      <SafeAreaView style={styles.container}>
-      <ListHeader/>
-        <FlatList
-          keyExtractor={(item) => item.id}
-          data={data}
-          renderItem={({item}) => (
-            <ListItem 
-              name={item.name} 
-              symbol={item.symbol}
-              currentPrice={item.current_price}
-              priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
-              logoUrl={item.image}
-              onPress={() => openModal(item)}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <SafeAreaView style={styles.container}>
+          <ListHeader />
+          <TextInput
+            style={styles.input}
+            placeholder="Search for some crypto"
+            placeholderTextColor="#C5C5C5"
+            value={searchText}
+            onChangeText={(t) => setSearchText(t)}
+          />
+          <FlatList
+            keyExtractor={(item) => item.id}
+            data={data}
+            renderItem={({ item }) => (
+              <ListItem
+                name={item.name}
+                symbol={item.symbol}
+                currentPrice={item.current_price}
+                priceChangePercentage7d={
+                  item.price_change_percentage_7d_in_currency
+                }
+                logoUrl={item.image}
+                onPress={() => openModal(item)}
+              />
+            )}
+          />
+        </SafeAreaView>
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+          style={styles.bottomSheet}
+        >
+          {selectedCoidData ? (
+            <Chart
+              currentPrice={selectedCoidData.current_price}
+              logoUrl={selectedCoidData.image}
+              name={selectedCoidData.name}
+              symbol={selectedCoidData.symbol}
+              priceChangePercentage7d={
+                selectedCoidData.price_change_percentage_7d_in_currency
+              }
+              sparkline={selectedCoidData.sparkline_in_7d.price}
             />
-          )}
-        />
-        
-      </SafeAreaView>
-      <BottomSheetModal
-        ref={bottomSheetModalRef}
-        index={0}
-        snapPoints={snapPoints}
-        style={styles.bottomSheet}
-      >
-        { selectedCoidData ? (
-        <Chart 
-          currentPrice={selectedCoidData.current_price}
-          logoUrl={selectedCoidData.image}
-          name={selectedCoidData.name}
-          symbol={selectedCoidData.symbol}
-          priceChangePercentage7d={selectedCoidData.price_change_percentage_7d_in_currency}
-          sparkline={selectedCoidData.sparkline_in_7d.price}
-        />
-        )
-        : null}
-      </BottomSheetModal>
-    </BottomSheetModalProvider>
+          ) : null}
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
 }
@@ -93,7 +117,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   titleWrapper: {
     marginTop: 40,
@@ -102,14 +126,14 @@ const styles = StyleSheet.create({
   largeTitle: {
     fontSize: 24,
     fontWeight: "bold",
-    textAlign: 'center',
+    textAlign: "center",
   },
-  divider: {
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#A9ABB1',
-    marginHorizontal: 16,
-    marginTop: 16,
-  },
+  // divider: {
+  //   height: StyleSheet.hairlineWidth,
+  //   backgroundColor: "#C5C5C5",
+  //   marginHorizontal: 16,
+  //   marginTop: 16,
+  // },
   bottomSheet: {
     shadowColor: "#000",
     shadowOffset: {
@@ -119,6 +143,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 40,
     elevation: 10,
-    backgroundColor: 'white',
+    backgroundColor: "white",
+  },
+  input: {
+    marginTop: 20,
+    backgroundColor: "#F6F6F6",
+    padding: 16,
+    fontSize: 16,
   },
 });
